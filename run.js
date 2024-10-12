bf1b1eb61b9606323c1a0b5f5ae0e4bb719a8714
miles453864 <29894500+NostalgicLorikeet@users.noreply.github.com>
2024-10-12 02:51:01 -0400

    THOSE WHO JOE

--- run.js
+++ run.js
@@ -5,13 +5,13 @@ function run() {
 	if (getRandomInt(0,2) == 0) {
 		caption.innerHTML = "when u fighting with someone and bro starts";
 		if (document.getElementById("includeExtendedVerbs").checked == "false") {
-			gen.innerHTML = demonyms[getRandomInt(0, demonyms.length - 1)] + " " + verbs[getRandomInt(0, verbs.length - 1)];
+			gen.innerHTML = demonyms[getRandomInt(0, demonyms.length)] + " " + verbs[getRandomInt(0, verbs.length)];
 		} else {
-			gen.innerHTML = demonyms[getRandomInt(0, demonyms.length - 1)] + " " + verbsExt[getRandomInt(0, verbsExt.length - 1)];
+			gen.innerHTML = demonyms[getRandomInt(0, demonyms.length)] + " " + verbsExt[getRandomInt(0, verbsExt.length)];
 		}
 	} else {
 		caption.innerHTML = "when u fighting with someone and bro starts to";
-		gen.innerHTML = demonyms[getRandomInt(0, demonyms.length - 1)] + " " + infinitives[getRandomInt(0, infinitives.length - 1)];
+		gen.innerHTML = demonyms[getRandomInt(0, demonyms.length)] + " " + infinitives[getRandomInt(0, infinitives.length)];
 	}
 }
 
